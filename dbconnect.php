612ea56f3833ced44b55b587341937763e9a41b1
joseph-j <jalbert.j@gmail.com>
2023-01-01 22:40:44 -0500

    Update and rename dbconnect.php to writedata.php
    Copy code from: https://microdigisoft.com/esp32-insert-data-into-mysql-database-using-php-and-arduino-ide/

--- dbconnect.php
+++
@@ -1,8 +0,0 @@
-<?php
-$MyUsername = "vapo";  // enter your username for mysql
-$MyPassword = "polar11!";  // enter your password for mysql
-$MyHostname = "localhost";      // this is usually "localhost" unless your database resides on a different server
-
-$dbh = mysql_pconnect($MyHostname , $MyUsername, $MyPassword);
-$selected = mysql_select_db("test",$dbh);
-?>
