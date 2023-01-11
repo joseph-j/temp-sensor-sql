c827158deb7b3a7ab70741662684f257745ed9d9
joseph-j <jalbert.j@gmail.com>
2023-01-10 21:15:30 -0500

    add data update

--- add_data.php
+++ add_data.php
@@ -2,13 +2,13 @@
 include("dbconnect.php");
 
 // Prepare the SQL statement
-$SQL = "INSERT INTO test.newdata (id ,Temperature ,Humidity) VALUES ('".$_GET["id"]."','".$_GET ["Temperature"]."','".$_GET["Humidity"]."')";   
+$SQL = "INSERT INTO test.ArduinoData (id ,Temperature ,Humidity) VALUES ('".$_GET["id"]."','".$_GET ["Temperature"]."','".$_GET["Humidity"]."')";   
 //$SQL1 = "INSERT INTO test.room (room) VALUES ('".$_GET["room"]."')";              
 
 // Execute SQL statement
 mysql_query($SQL);
 //mysql_query($SQL1);
-//nclude("select_room.php");
+//include("select_room.php");
 
 
 // Go to the review_data.php (optional)
