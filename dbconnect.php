8a93e3fa020a69a71a489e5d690d5605c2efcf98
joseph-j <jalbert.j@gmail.com>
2023-01-10 19:54:57 -0500

    updating dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,6 +1,6 @@
 <?php
-$MyUsername = "vapo";  // enter your username for mysql
-$MyPassword = "polar11!";  // enter your password for mysql
+$MyUsername = "root";  // enter your username for mysql
+$MyPassword = "PASSWORD";  // enter your password for mysql
 $MyHostname = "localhost";      // this is usually "localhost" unless your database resides on a different server
 
 $dbh = mysql_pconnect($MyHostname , $MyUsername, $MyPassword);
