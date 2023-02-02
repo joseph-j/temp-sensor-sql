fd1e0ab3f774ff7ba7460a43de25e130a0154bc1
joseph-j <jalbert.j@gmail.com>
2023-02-01 22:08:49 -0500

    updated with real credentials.

--- readdata.php
+++ readdata.php
@@ -2,31 +2,43 @@
 <html><body>
 
 <?php
-require_once ("db.php");
+echo "Connecting to Database... <br>";
+
+//Set Connection Details
+$MyUsername = "root";  // enter your username for mysql
+$MyPassword = "PASSWORD";  // enter your password for mysql
+$MyHostname = "localhost";      // this is usually "localhost" unless your database resides on a different server
+$DB = "test";
+echo "variables set...".$MyUsername.", ".$MyPassword.", ".$MyHostname.", ".$DB."<br>";
+
+//Connect to Database
+$dbh = mysqli_connect( $MyHostname, $MyUsername, $MyPassword, $DB) or die('not connecting<br>');
+if (mysqli_connect_errno()) {
+  echo "Failed to connect to MySQL: " . mysqli_connet_errno();
+  exit();
+}
+echo "hooked in<br>";
+
+//Select Database (redundant?)
+$selected = mysqli_select_db($dbh, "test");
 
 
-$sql = "SELECT id, SensorName, location, Temperature, Humidity,Pressure,reading_time FROM esp32data ORDER BY id DESC";
+$sql = "SELECT reading_time, Temperature, Humidity, ExTemp FROM ArduinoData ORDER BY reading_time DESC";
 
 echo '<table cellspacing="5" cellpadding="5">
       <tr> 
-        <td>ID</td> 
-        <td>SensorName</td> 
-        <td>Location</td> 
+        <td>reading_time</td>  
         <td>Temprature</td> 
         <td>Humidity</td>
-	<td>Pressure</td>
-        <td>Timestamp</td> 
+	<td>ExTemp</td>
       </tr>';
  
 if ($result = $conn->query($sql)) {
     while ($row = $result->fetch_assoc()) {
-        $row_id = $row["id"];
-        $row_sensor = $row["SensorName"];
-        $row_location = $row["location"];
+        $row_reading_time = $row["reading_time"];
         $row_value1 = $row["Temperature"];
         $row_value2 = $row["Humidity"]; 
-	$row_value3 = $row["Pressure"]; 
-        $row_reading_time = $row["reading_time"];
+	$row_value3 = $row["ExTemp"]; 
         // Uncomment to set timezone to - 1 hour (you can change 1 to any number)
         //$row_reading_time = date("Y-m-d H:i:s", strtotime("$row_reading_time - 1 hours"));
       
@@ -34,13 +46,10 @@ if ($result = $conn->query($sql)) {
         //$row_reading_time = date("Y-m-d H:i:s", strtotime("$row_reading_time + 4 hours"));
       
         echo '<tr> 
-                <td>' . $row_id . '</td> 
-                <td>' . $row_sensor . '</td> 
-                <td>' . $row_location . '</td> 
+                <td>' . $row_reading_time . '</td> 
                 <td>' . $row_value1 . '</td> 
                 <td>' . $row_value2 . '</td>
 		<td>' . $row_value3 . '</td>
-                <td>' . $row_reading_time . '</td> 
               </tr>';
     }
     $result->free();
